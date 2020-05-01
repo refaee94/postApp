57a898f59af13ebc42dfbfe4b2fc227d3980c3dd
mohammed <m.refaee123580@gmail.com>
2020-05-02 02:28:13 +0400

    Update file.js

--- middleware/file.js
+++ middleware/file.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const path = require("path");
 const storage = multer.diskStorage({
   destination: (req, myfile, cd) => {
-    cd(null, "backend/images");
+    cd(null, "images");
   },
   filename: (req, myfile, cd) => {
     const name = myfile.fieldname.toLowerCase().split(" ").join("-");
